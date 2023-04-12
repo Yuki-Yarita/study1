10b6361823a334ccf08071a99d4a29c1d656dae3
Yuki Yarita <yariyuu45@gmail.com>
2023-04-12 11:17:05 +0900

    real project first commitgit add .! unko

---
+++ src/main/java/study1/Study1.java
@@ -0,0 +1,41 @@
+package study1;
+
+import java.util.Iterator;
+
+public class Study1 {
+	public static void main(String[] args) {
+		String string = "this is String";
+		short s = 100; 
+		int number = 100;
+		char c = 'A';
+		float f = 3.14f;
+		double d = 3.14;
+		
+		
+		int num1 = 100;
+		int num2 = 200;
+		boolean result = true;
+		
+		
+		int tttt = 1;
+		Integer num3 = null; // null 
+
+		
+		if(result) {
+			for (int i = 0; i < 5; i++) {
+				System.out.println(num1 + num2);
+			}
+		}else {
+			System.out.println("FUCK");
+		}
+		
+		switch(tttt) {
+		case 1 : System.out.println("this is 1");
+		case 2 : System.out.println("this is 2");
+		}
+		
+		
+		int nnum = 777;
+	}
+	
+}
